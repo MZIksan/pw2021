6642517b6cbde0ab7af6674858d81ae70cf36ab4
MZIksan <mziksan10@gmail.com>
2021-03-26 22:36:38 +0700

    menambahkan fitur login

---
+++ Kuliah/Pertemuan12/functions.php
@@ -0,0 +1,89 @@
+<?php
+function Koneksi()
+{
+  return mysqli_connect('localhost', 'root', '', 'pw_2020');
+}
+
+function Query($query)
+{
+  $conn = Koneksi();
+  $result = mysqli_query($conn, $query);
+  // Jika hasilnya hanya 1 data
+  if (mysqli_num_rows($result) == 1) {
+    return mysqli_fetch_assoc($result);
+  }
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
+
+function Tambah($data)
+{
+  $conn = Koneksi();
+
+  $nama = htmlspecialchars($data['nama']);
+  $npm = htmlspecialchars($data['npm']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "insert into mahasiswa values (null, '$nama','$npm','$email','$jurusan','$gambar')";
+  mysqli_query($conn, $query) or die(mysqli_error($conn));
+  return mysqli_affected_rows($conn);
+}
+
+function Hapus($id)
+{
+  $conn = Koneksi();
+  mysqli_query($conn, "delete from mahasiswa where id = $id") or die(mysqli_error($conn));
+  return mysqli_affected_rows($conn);
+}
+
+function Ubah($data)
+{
+  $conn = Koneksi();
+
+  $id = $data['id'];
+  $nama = htmlspecialchars($data['nama']);
+  $npm = htmlspecialchars($data['npm']);
+  $email = htmlspecialchars($data['email']);
+  $jurusan = htmlspecialchars($data['jurusan']);
+  $gambar = htmlspecialchars($data['gambar']);
+
+  $query = "update mahasiswa set nama='$nama', npm='$npm', email='$email', jurusan='$jurusan', gambar='$gambar' where id=$id";
+  mysqli_query($conn, $query) or die(mysqli_error($conn));
+  return mysqli_affected_rows($conn);
+}
+
+function cari($keyword)
+{
+  $conn = Koneksi();
+  $query = "select * from mahasiswa where nama like '%$keyword%' or npm like '%$keyword%' ";
+  $result = mysqli_query($conn, $query);
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
+
+function login($data)
+{
+  $conn = Koneksi();
+  $username = htmlspecialchars($data['username']);
+  $password = htmlspecialchars($data['password']);
+
+  if ("select * from user where username = '$username' && password = '$password'") {
+    // set session
+    $_SESSION['login'] = true;
+    header("location: index.php");
+    exit;
+  } else {
+    return [
+      'error' => true,
+      'pesan' => 'Username / Password salah!'
+    ];
+  }
+}
---
+++ Kuliah/Pertemuan12/hapus.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+if (!isset($_SESSION['login'])) {
+  header('location: login.php');
+}
+
+require 'functions.php';
+// Jika tidak ada id URL
+if (!isset($_GET['id'])) {
+  header("location: index.php");
+  exit;
+}
+// Mengambil id dari URL
+$id = $_GET['id'];
+if (Hapus($id) > 0) {
+  echo "<script>
+  alert('data berhasil dihapus!')
+  document.location.href ='index.php'
+  </script>";
+} else {
+  echo "Data gagal dihapus!";
+}
